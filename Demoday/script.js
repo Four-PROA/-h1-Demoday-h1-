bfa8e69a8d57a83544954eaf6372e12395cac733
Ramon Diego <ramondiego857@gmail.com>
2021-11-10 13:11:04 -0300

    animações

--- Demoday/script.js
+++ Demoday/script.js
@@ -17,17 +17,19 @@ const debounce = function(func, wait, immediate) {
   
   const target = document.querySelectorAll('[data-step]');
   const animationClass = 'animate';
-  
+ 
   function animeScroll() {
     const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+
     target.forEach(function(element) {
       if((windowTop) > element.offsetTop) {
         element.classList.add(animationClass);
       } else {
         element.classList.remove(animationClass);
       }
-    })
-  }
+    })   
+}
+
   
   animeScroll();
   
